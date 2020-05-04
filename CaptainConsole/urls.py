051d0b4919eab83944bd7ba323d226764c6c9a2d
tonysalva <antonioksalvador@gmail.com>
2020-05-04 15:35:18 +0000

    manufacturers -> consoles

--- CaptainConsole/urls.py
+++ CaptainConsole/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('', include('item.urls')),
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
-    path('manufacturers/', include('console.urls')),
+    path('consoles/', include('console.urls')),
 
 ]
