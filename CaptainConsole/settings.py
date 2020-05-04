af1b6718e5c25986d7a30ded9cbe4fec596b7349
vesteinnbjarna <vesteinn19@ru.is>
2020-05-04 12:34:06 +0000

    Added Database connection to settings.py #TODO makemigrate when we have have finished the models :)

--- CaptainConsole/settings.py
+++ CaptainConsole/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'CaptainConsole.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nuchowyz',
+        'USER': 'nuchowyz',
+        'PASSWORD': 'lESEciCzztTUX-BLF-wqzknuKXSlvpaC',
+        'HOST': 'balarama.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
