78662a4e459184aa926bab71a4450f299adff955
tonysalva <antonioksalvador@gmail.com>
2020-05-05 15:00:45 +0000

    admin page

--- item/admin.py
+++ item/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Item
+from .models import Item, ItemImage, ItemCategory
 
 admin.site.register(Item)
+admin.site.register(ItemImage)
+admin.site.register(ItemCategory)
