e0713ff8563c95f0982441bad0e0ecde522bb070
Akzelele <54936879+Akzelele@users.noreply.github.com>
2020-05-05 13:18:05 +0000

    commit4pull

--- CaptainConsole/urls.py
+++ CaptainConsole/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('', include('item.urls')),
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
-    path('consoles/', include('console.urls')),
+    path('', include('console.urls')),
 
 ]
--- console/views.py
+++ console/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from console.models import Console
 
 
@@ -6,3 +6,9 @@ from console.models import Console
 def index(request):
     context = {'consoles': Console.objects.all().order_by('name')}
     return render(request, 'console/index.html', context)
+
+
+def get_console_by_id(request, id):
+    return render(request, 'console/single_console.html', {
+        'console    ': get_object_or_404(Console, pk=id)
+    })
