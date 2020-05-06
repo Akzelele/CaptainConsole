0c7b7b1b6e0f86a7374811413156c34fd69e67a8
Bryni eplabaka <brynjolfur19@ru.is>
2020-05-06 12:58:31 +0000

    núna er hægt að smella á vörur til þess að skoða nánari upplýsingar um þær

--- console/urls.py
+++ console/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('<int:id>', views.index, name="console-index"),
-
 ]
--- item/views.py
+++ item/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from item.models import Item
 
 
@@ -6,3 +6,8 @@ from item.models import Item
 def index(request):
     context = {'items': Item.objects.all().order_by('id')}
     return render(request, 'item/index.html', context)
+
+def get_item_by_id(request, id):
+    return render(request, 'item/item_details.html', {
+        'item': get_object_or_404(Item, pk=id)
+    })
