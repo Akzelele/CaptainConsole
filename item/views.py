279fdfa935b06496852d5833adf84adcf4df50fb
Akzelele <54936879+Akzelele@users.noreply.github.com>
2020-05-06 15:12:06 +0000

    FOKK JÁ, autodropperpopperpogger

--- item/views.py
+++ item/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+
+from helper.context_helper import build_context
 from item.models import Item
 
 
 # Create your views here.
 def index(request):
-    context = {'items': Item.objects.all().order_by('id')}
+    context = build_context()
     return render(request, 'item/index.html', context)
 
 def get_item_by_id(request, id):
