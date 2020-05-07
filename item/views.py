63b5d21ea5b0e5302fb2fea10c3a24a339057886
tonysalva <antonioksalvador@gmail.com>
2020-05-07 20:55:39 +0000

    search bar, virkar ekki(ennþá) :S

--- item/views.py
+++ item/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.http import JsonResponse
 from helper.context_helper import build_context
 from item.models import Item
 
 
 # Create your views here.
 def index(request):
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        items = [{
+            'id': x.id,
+            'name': x.name,
+            'description': x.description,
+            'firstImage': x.itemimage_set.first().image
+        } for x in Item.objects.filter(name__icontains=search_filter)]
+        return JsonResponse({'data': items})
     context = build_context()
     return render(request, 'item/index.html', context)
 
