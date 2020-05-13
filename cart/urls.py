804285e5d13d94bc7b2f106fee43cec68a404719
Akzelele <54936879+Akzelele@users.noreply.github.com>
2020-05-13 15:21:15 +0000

    Update urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import (
     index,
     post_item_list,
-    CheckoutView)
+    checkout_view)
 
 urlpatterns = [
-    path('', views.index, name="cart-index"),
-    path('items', views.post_item_list, name="item-list"),
-    path('checkout', views.checkout_view, name="checkout_view")
+    path('', index, name="cart-index"),
+    path('items', post_item_list, name="item-list"),
+    path('checkout', checkout_view, name="checkout_view")
 ]
