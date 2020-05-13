5c1178ed5e6ab102e635af34f719f840e8cf69e5
vesteinnbjarna <vesteinn19@ru.is>
2020-05-13 18:36:39 +0000

    changed one column of the usersearchhistory table

--- user/models.py
+++ user/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.CharField(max_length=9999)
 
+
 class UserSearchHistory(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
