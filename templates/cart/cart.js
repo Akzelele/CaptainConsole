a59bf14886b00457c3da026fbc91682c24b35814
Brynni <54936233+Brynni@users.noreply.github.com>
2020-05-14 21:49:32 +0000

    Delete cart.js

--- templates/cart/cart.js
+++
@@ -1,6 +0,0 @@
-function CartOpener(){
-        var item_storage = Object.entries(localStorage);
-        console.log(item_storage)
-        console.log("were gonna be okay")
-        return(item_storage);
-    }
