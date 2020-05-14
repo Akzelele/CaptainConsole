17a2c07310335162203b47926ce29ff58b3e3104
Akzelele <54936879+Akzelele@users.noreply.github.com>
2020-05-14 14:02:51 +0000

    fancier stuff

--- cart/forms.py
+++ cart/forms.py
@@ -4,14 +4,15 @@ from django_countries.fields import CountryField
 
 
 class ContactForm(forms.Form):
-    first_name = forms.CharField(max_length=255)
-    last_name = forms.CharField(max_length=255)
-    email = forms.EmailField()
-    country = CountryField().formfield()
-    street_name = forms.CharField(max_length=255)
-    house_number = forms.IntegerField(min_value=0, max_value=100000)  # thank you UK for min and Brazil for max
-    city = forms.CharField(max_length=255)
-    zip = forms.CharField(max_length=10)
+    first_name = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "First name"}), max_length=255)
+    last_name = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "Last name"}), max_length=255)
+    email = forms.EmailField(label="", widget=forms.EmailInput(attrs={'placeholder': "Email"}), max_length=255)
+    country = CountryField(blank_label='Select country').formfield(label='')
+    street_name = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "Street name"}), max_length=255)
+    house_number = forms.IntegerField(
+        min_value=0, label="", max_value=100000, widget=forms.NumberInput(attrs={'placeholder':'House number'}))
+    city = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "City"}), max_length=255)
+    zip = forms.CharField(label="", widget=forms.NumberInput(attrs={'placeholder': 'Zip code'}), max_length=10)
 
 
 class PaymentForm(forms.Form):
@@ -29,10 +30,12 @@ class PaymentForm(forms.Form):
         (11, "November"),
         (12, "December"),
     )
-    cardholder_name = forms.CharField(max_length=255)
-    card_number = forms.CharField(widget=forms.NumberInput(attrs={'placeholder': 'Card Number'}))
-    expiration_date_month = forms.ChoiceField(choices=MONTHS)
-    expiration_date_year = forms.CharField(max_length=4)
-    CVC = forms.CharField(max_length=3)
+    cardholder_name = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "Cardholder's name"}),
+                                      max_length=255)
+    card_number = forms.CharField(min_length=16, max_length=16, label="",
+                                  widget=forms.NumberInput(attrs={'placeholder': 'Card number'}))
+    expiration_month = forms.ChoiceField(choices=MONTHS, label="", widget=forms.Select())
+    expiration_year = forms.CharField(label="", widget=forms.NumberInput(attrs={'placeholder': '2020'}), max_length=4)
+    CVC = forms.CharField(label="", widget=forms.NumberInput(attrs={'placeholder': 'CVC'}), max_length=3)
 
     # widgets numberinput
