de0493531796c8027c864471d70ac24ec6d7839b
tonysalva <antonioksalvador@gmail.com>
2020-05-14 20:51:05 +0000

    sort button optimization(vantar myndir samt :/)

--- item/views.py
+++ item/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from helper.context_helper import build_context, build_item_context
 from item.models import Item
-
+import operator
 
 # Create your views here.
+
 def index(request):
+
+    context = build_context()
+
     if 'search_filter' in request.GET:
         search_filter = request.GET['search_filter']
         items = [{
@@ -21,32 +25,34 @@ def index(request):
 
         if sort_filter == 'name':
             items = [{
-                'id': x.id,
-                'name': x.name,
-                'price': x.price,
-                'firstImage': x.itemimage_set.first().image
-            } for x in Item.objects.all().order_by('name')]
+                'id': context['items'][x]['id'],
+                'name': context['items'][x]['name'],
+                'price': context['items'][x]['price'],
+                'firstImage': context['items'][x]['itemimage']
+            } for x in range (len(context['items']))]
+            items.sort(key=operator.itemgetter('name'))
             return JsonResponse({'data': items})
 
         if sort_filter == 'pricelhbtn':
             items = [{
-                'id': x.id,
-                'name': x.name,
-                'price': x.price,
-                'firstImage': x.itemimage_set.first().image
-            } for x in Item.objects.all().order_by('price')]
+                'id': context['items'][x]['id'],
+                'name': context['items'][x]['name'],
+                'price': context['items'][x]['price'],
+                'firstImage': context['items'][x]['itemimage']
+            } for x in range (len(context['items']))]
+            items.sort(key=operator.itemgetter('price'))
             return JsonResponse({'data': items})
 
         if sort_filter == 'pricehlbtn':
             items = [{
-                'id': x.id,
-                'name': x.name,
-                'price': x.price,
-                'firstImage': x.itemimage_set.first().image
-            } for x in Item.objects.all().order_by('-price')]
+                'id': context['items'][x]['id'],
+                'name': context['items'][x]['name'],
+                'price': context['items'][x]['price'],
+                'firstImage': context['items'][x]['itemimage']
+            } for x in range (len(context['items']))]
+            items.sort(key=operator.itemgetter('price'),reverse=True)
             return JsonResponse({'data': items})
 
-    context = build_context()
     return render(request, 'item/index.html', context)
 
 
