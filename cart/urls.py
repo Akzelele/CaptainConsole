48d5657ed46648dabb236d9c905d4b027b24c164
vesteinnbjarna <vesteinn19@ru.is>
2020-05-15 12:40:07 +0000

    added order-review.html, a site after an order has been created

--- cart/urls.py
+++ cart/urls.py
@@ -3,13 +3,15 @@ from .views import (
     index,
     post_item_list,
     contact_view,
-    payment_view, review_view)
+    payment_view,
+    review_view,
+    order_review)
 
 urlpatterns = [
     path('', index, name="cart-index"),
     path('items', post_item_list, name="item-list"),
     path('contact', contact_view, name="contact_view"),
     path('payment', payment_view, name="payment_view"),
-    path('review', review_view, name="review_view")
-
+    path('review', review_view, name="review_view"),
+    path('order-review', order_review, name="order_review")
 ]
